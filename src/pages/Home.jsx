1bab79a278bf47df16756023f041abcac894c7ea
Anil Rai <anilrai4001@gmail.com>
2024-02-17 01:31:02 +0530

    last commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,7 +21,7 @@ function Home() {
                     <div className="flex flex-wrap">
                         <div className="p-2 w-full">
                             <h1 className="text-2xl font-bold text-white">
-                                No Posts
+                                Login to view Posts
                             </h1>
                         </div>
                     </div>
