16f2c24dc0829d33b9cd32c9fd81ee1887121192
Anil Rai <anilrai4001@gmail.com>
2024-02-17 01:26:45 +0530

    updated image-size

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -33,13 +33,14 @@ export default function Post() {
   };
 
   return post ? (
-    <div className="py-8 bg-gray-400">
-      <Container>
-        <div className="w-full flex justify-center mb-4 relative border rounded-xl p-2">
+    <div className="py-8 bg-gray-400 ">
+      <Container >
+        <div className="mb-4 flex justify-center relative  p-2 ">
           <img
             src={appwriteService.getFilePreview(post.featuredImage)}
             alt={post.title}
-            className="rounded-xl"
+            className="rounded-xl w-2/4"
+
           />
 
           {isAuthor && (
@@ -55,10 +56,13 @@ export default function Post() {
             </div>
           )}
         </div>
-        <div className="w-full mb-6 ">
-          <h1 className="text-2xl font-bold">{post.title}</h1>
-        </div>
-        <div className="browser-css">{parse(post.content)}</div>
+
+          <div className="mb-6 ">
+            <h1 className="text-2xl font-bold">{post.title}</h1>
+          </div>
+
+          <div className="browser-css">{parse(post.content)}</div>
+ 
       </Container>
     </div>
   ) : null;
